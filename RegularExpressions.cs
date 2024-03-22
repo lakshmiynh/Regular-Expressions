5f8d905e3113c11074567bf81571ea40987d8526
Lakshmi <lakshmiynh8@gmail.com>
2024-03-22 20:03:12 +0530

    Added:functionality for validate users first and last name

--- RegularExpressions.cs
+++ RegularExpressions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -8,24 +9,40 @@ using System.Xml.Linq;
 
 namespace RgularExpression
 {
-    class RegularExpressions
+    public class RegularExpressions
     {
-        static void Main(String[] args)
+        public string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+
+        static void Main(string[] args)
         {
             Console.WriteLine("Enter your First Name");
-            string firstname=Console.ReadLine();
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter your Last name");
+            string lastname = Console.ReadLine();
 
-            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+            // Create an instance of RegularExpressions
+            RegularExpressions regex = new RegularExpressions();
 
-            // Check if the name matches the pattern
-            if( Regex.IsMatch(firstname, pattern))
+            // Call validation method for both first and last name
+            regex.Validation(firstname);
+            regex.Validation(lastname);
+        }
+
+        // Changed method to non-static
+        public void Validation(string name)
+        {
+            // Access the pattern through instance variable
+            if (Regex.IsMatch(name, this.pattern))
             {
                 Console.WriteLine("Valid");
             }
             else
             {
-                Console.WriteLine("please enter the valid name");
+                Console.WriteLine("Please enter a valid name");
             }
         }
-    }
+
+    }  
+        
+    
 }
